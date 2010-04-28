9c82c147c1d624418730fbabe0aa06b136593fcc
andreev <d.g.andreev@gmail.com>
2010-04-28 11:21:19 +0400

    Added ability to perform the following operations: OPERATION, CHECK, CANCEL, CONFIRM

--- lib/eport.rb
+++ lib/eport.rb
@@ -22,27 +22,7 @@ class Eport
     cp.new_catalog(data)    
   end  
   
-  def do_refill_operation(operation_id, product_id, value, account)
-    req = Request.new do |r|
-    r.point = @config[:eport][:point]
-    r.private_key_path = "#{RAILS_ROOT}/#{@config[:eport][:private_key_path]}"
-
-    r.host = @config[:eport][:host]
-    r.path = "/cp/fe"
-   
-    req = EportRequestPlainText.new
-    req.operation_type = "OPERATION"
-    req.operation_id   = operation_id
-    req.product_id     = product_id
-    req.value          = value
-    req.account        = account
-    r.body = req.body
-   end
-   ic = Iconv.new('UTF-8', 'WINDOWS-1251')
-   ic.iconv(req.confirm)
-  end
-  
-  def confirm_refill_operation(operation_id, product_id, value, account)
+  def refill_operation(operation_name, operation_id, product_id, value, account)
      req = Request.new do |r|
      r.point = @config[:eport][:point]
      r.private_key_path = "#{RAILS_ROOT}/#{@config[:eport][:private_key_path]}"
@@ -51,12 +31,12 @@ class Eport
      r.path = "/cp/fe"
 
      req = EportRequestPlainText.new
-     req.operation_type = "CONFIRM"
+
+     req.operation_type = operation_name.to_s.upcase if [:operation, :confirm, :check, :cancel].include?(operation_name)
      req.operation_id   = operation_id
      req.product_id     = product_id
      req.value          = value
      req.account        = account
-
      r.body = req.body
     end
     ic = Iconv.new('UTF-8', 'WINDOWS-1251')
